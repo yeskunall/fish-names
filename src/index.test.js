b6cac83fc82b21afa7fd861ea16efdc882b9ef87
Kunall Banerjee <yeskunall96@gmail.com>
2017-01-03 01:35:31 -0500

    More test coverage.

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,9 @@
 const fishNames = require('./index');
 const expect = require('chai').expect;
 
+const maleFish = require('./male-fish.json');
+const femaleFish = require('./female-fish.json');
+
 describe('fish-names', () => {
   describe('all', () => {
     it('should be an array of strings', () => {
@@ -19,6 +22,18 @@ describe('fish-names', () => {
     it('should return a random fish name from `fishNames.all`', () => {
       const randomFish = fishNames.random();
       expect(fishNames.all).to.include(randomFish);
+    });
+  });
+
+  describe('allMaleFish', () => {
+    it('should return a list of all male fish', () => {
+      expect(fishNames.allMaleFish).to.not.include(fishNames.allFemaleFish);
+    });
+  });
+
+  describe('allFemaleFish', () => {
+    it('should return a list of all female fish', () => {
+      expect(fishNames.allFemaleFish).to.not.include(fishNames.allMaleFish);
     });
   });
 });
