cafdd6599ca8c6f8922eaa1a462beb1c489e5521
Kunall Banerjee <yeskunall96@gmail.com>
2017-01-03 03:25:02 -0500

    feat(random): Add ability to get an array of random fish names
    Passing a number (n) as a param to the random function will return an array of random fish names of
    
    length `n`
    
    Closes #2.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ const femaleFish = require('./female-fish.json');
 const uniqueRandomArray = require('unique-random-array');
 
 const allFish = maleFish.concat(femaleFish);
+const getRandomFish = uniqueRandomArray(allFish);
+
+const random = function(n) {
+  if (n === undefined) return getRandomFish();
+  const randomFish = [];
+  for (var i = 0; i < n; i++) {
+    randomFish.push(getRandomFish());
+  }
+  return randomFish;
+};
 
 module.exports = {
   all: allFish,
   allMaleFish: maleFish,
   allFemaleFish: femaleFish,
-  random: uniqueRandomArray(allFish)
+  random: random
 };
--- src/index.test.js
+++ src/index.test.js
@@ -6,11 +6,11 @@ const femaleFish = require('./female-fish.json');
 
 describe('fish-names', () => {
   describe('all', () => {
-    it('should be an array of strings', () => {
+    it('should be an array of strings', function() {
       expect(fishNames.all).to.satisfy(isArrayOfStrings);
 
       function isArrayOfStrings(arr) {
-        return arr.every((item) => {
+        return arr.every(function(item) {
           return typeof item === 'string';
         });
       }
@@ -18,20 +18,28 @@ describe('fish-names', () => {
     });
   });
 
-  describe('random', () => {
+  describe('random', function() {
     it('should return a random fish name from `fishNames.all`', () => {
       const randomFish = fishNames.random();
       expect(fishNames.all).to.include(randomFish);
     });
+
+    it('should return an array of random fish names if passed a number as a param', function() {
+      const randomFish = fishNames.random(3);
+      expect(randomFish).to.have.length(3);
+      randomFish.forEach(function(fish) {
+        expect(fishNames.all).to.include(fish);
+      });
+    });
   });
 
-  describe('allMaleFish', () => {
+  describe('allMaleFish', function() {
     it('should return a list of all male fish', () => {
       expect(fishNames.allMaleFish).to.not.include(fishNames.allFemaleFish);
     });
   });
 
-  describe('allFemaleFish', () => {
+  describe('allFemaleFish', function() {
     it('should return a list of all female fish', () => {
       expect(fishNames.allFemaleFish).to.not.include(fishNames.allMaleFish);
     });
